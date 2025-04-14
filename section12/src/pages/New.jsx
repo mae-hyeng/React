625b91fad1282ce920f22e88e8ea42e5e58d5737
mae-hyeng <topjhoh20@naver.com>
2025-04-14 16:31:57 +0900

    글작성 페이지 기능 구현 - 새로 작성될 데이터들을 onChangeInput 함수에서 name과 value 값으로 받아온 뒤 input을 최신화 시켜줌 - 취소하기, 작성완료 버튼 이벤트 구현

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,15 +1,27 @@
 import Editor from "../components/Editor";
 import Header from "../components/Header";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+    // replace : 뒤로가기 방지하면서 페이지를 이동시킴
+  };
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
