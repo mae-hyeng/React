b85ddfeda858be17d82927b4b023190e094333dd
mae-hyeng <topjhoh20@naver.com>
2025-04-15 16:23:42 +0900

    - localStorage 사용해서 데이터 저장 기능 추가 - 데이터 불러오기 기능 추가 - id값 Max값으로 설정 기능 추가 - data를 불러오기 이전에 렌더링 될 때 오류 발생하는 것을 방지하기 위해 isLoading 생성

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,6 +1,13 @@
 import "./App.css";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import { useReducer, useRef, useContext, createContext } from "react";
+import {
+  useReducer,
+  useRef,
+  useContext,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
 
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
@@ -13,40 +20,30 @@ import Header from "./components/Header";
 
 // import { getEmotionImage } from "./util/get-emotion-image";
 
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date("2025-04-13").getTime(),
-    emotionId: 1,
-    content: "1번 일기 내용",
-  },
-  {
-    id: 2,
-    createdDate: new Date("2025-04-12").getTime(),
-    emotionId: 2,
-    content: "2번 일기 내용",
-  },
-  {
-    id: 3,
-    createdDate: new Date("2025-03-12").getTime(),
-    emotionId: 3,
-    content: "3번 일기 내용",
-  },
-];
-
 function reducer(state, action) {
+  let nextState;
+
   switch (action.type) {
+    case "INIT":
+      return action.data;
     case "CREATE":
-      return [action.data, ...state];
+      nextState = [action.data, ...state];
+      break;
     case "UPDATE":
-      return state.map((item) =>
+      nextState = state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item
       );
+      break;
     case "DELETE":
-      return state.filter((item) => String(item.id) !== String(action.id));
+      nextState = state.filter((item) => String(item.id) !== String(action.id));
+      break;
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(nextState));
+
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
@@ -56,14 +53,39 @@ export const DiaryDispatchContext = createContext();
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  // const nav = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
 
-  // const onClickButton = () => {
-  //   nav("/new");
-  // };
+  useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if (!storedData) {
+      return;
+    }
 
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+    const parsedData = JSON.parse(storedData);
+
+    if (!Array.isArray(parsedData)) {
+      setIsLoading(false);
+      return;
+    }
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id);
+      }
+    });
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: "INIT",
+      data: parsedData,
+    });
+
+    setIsLoading(false);
+  }, []);
 
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
@@ -99,6 +121,10 @@ function App() {
       id,
     });
   };
+
+  if (isLoading) {
+    return <div>데이터 로딩중입니다...</div>;
+  }
 
   return (
     <>
